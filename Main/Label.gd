43b7e83c2965231aa8a831a3d885c59c6ada2206
richardbonneau <richardbonneau91@gmail.com>
2022-03-12 15:58:12 -0500

    second zone and UI and ready for itch release

--- Main/Label.gd
+++ Main/Label.gd
@@ -7,8 +7,8 @@ func _ready():
 	WorldShifter.defineStatusUI(self)
 
 func _process(delta):
-	if(showMushTimer): text = String(int($MushTimer.time_left))
-	elif(showRecoveryTimer): text = String(int($RecoveryTimer.time_left))
+	if(showMushTimer): text = "???\n"+String(int($MushTimer.time_left))
+	elif(showRecoveryTimer): text = "Recovering\n"+String(int($RecoveryTimer.time_left))
 
 func _on_MushTimer_timeout():
 	print("_on_MushTimer_timeout")
---
+++ _menu/menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+onready var is_fullscreen = false
+
+func _full_screen_button():
+	if !is_fullscreen:
+		$Button.text = "Windowed Screen"
+		is_fullscreen = true
+	else: 
+		$Button.text = "Full Screen (Recommended)"
+		is_fullscreen = false
+	OS.window_fullscreen = !OS.window_fullscreen
+
+func _windowed_button():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	get_tree().change_scene("res://Main/Main.tscn")
